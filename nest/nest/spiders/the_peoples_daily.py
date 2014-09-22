03bb3df51c320d81ebd8c8344fe86e1f0fe2cc91
Skillachie <dwaynecampbell13@gmail.com>
2014-09-22 05:33:24 -0400

    completed most of the peoples daily of china spider

--- nest/nest/spiders/the_peoples_daily.py
+++ nest/nest/spiders/the_peoples_daily.py
@@ -1,16 +1,15 @@
 import scrapy
 import re
 from scrapy.contrib.spiders import CrawlSpider, Rule
-from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 from scrapy import log
-from pprint import pprint
 from scrapy.selector import HtmlXPathSelector
 from scrapy.contrib.linkextractors.lxmlhtml import LxmlLinkExtractor
+from nest.items import ArticleItem, ArticleMetaInformation
+from scrapy.selector import Selector
 
 #TODO implement throttle
 
 class PeopleDailySpider(CrawlSpider):
-
 
 	name = "the_peoples_daily"
 	allowed_domains = ["english.people.com.cn"]
@@ -32,8 +31,38 @@ class PeopleDailySpider(CrawlSpider):
 
 	def parse_follow(self, response):
 
-		#TODO save html and send to RabbitMQ here
-		#TODO complete pipeline and items
-		#print(response.body)
-		pass
+		article_item = ArticleItem()
+		article_meta_information = ArticleMetaInformation()
 
+		article_item['source_link'] = response.url
+		article_item['time_of_crawl'] = response.headers['Date']
+		article_item['html_content'] = response.body
+		article_item['source_type'] = 'news_article' #TODO  should be enum
+		
+		article_item['title'] = response.xpath('//title/text()').extract()
+
+		article_meta_information['date_published'] = response.xpath('//meta[@name=\'publishdate\']/@content').extract()
+		article_meta_information['location'] = 'china' #TODO should be enum
+
+		if response.xpath('//div[contains(@class, "wb_4 clear")]/text()').re(r'Editor:\s*(.*)'):
+			raw_authors = response.xpath('//div[contains(@class, "wb_4 clear")]/text()').re(r'Editor:\s*(.*)')
+			article_meta_information['author'] = self.get_authors(raw_authors)
+		else:
+			raw_authors = response.xpath('//div[contains(@class, "wb_13 clear")]/text()').re(r'Editor:\s*(.*)')
+			article_meta_information['author'] = self.get_authors(raw_authors)
+
+
+		keywords = response.xpath('//meta[@name=\'keywords\']/@content').extract()
+		article_meta_information['topics'] = self.get_topics(keywords)
+		article_meta_information['language'] = 'zh' #http://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
+		article_item['meta_information'] = article_meta_information
+	
+	def get_topics(self,keywords):
+		
+		#data always present in 0 of array
+		for keyword_list in keywords:
+			return keyword_list.split(',')
+
+
+	def get_authors(self,raw_authors):
+		return  [raw_author for raw_author in raw_authors]  #TODO Encoding for raw author
