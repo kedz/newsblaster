c1252cd64e2175a7a09450dda42e282bfb667a3a
Skillachie <dwaynecampbell13@gmail.com>
2014-09-22 05:32:47 -0400

    article model

--- nest/nest/items.py
+++ nest/nest/items.py
@@ -5,10 +5,22 @@
 # See documentation in:
 # http://doc.scrapy.org/en/latest/topics/items.html
 
-import scrapy
+from scrapy.item import Item, Field
+
+class ArticleItem(Item):
+	title = Field()
+	source_type = Field()
+	source_link = Field()
+	time_of_crawl = Field()
+	html_content = Field()
+	text_content = Field()
+	meta_information = Field() # ArticleMetaInformation Item
 
 
-class NestItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class ArticleMetaInformation(Item):
+	location = Field()
+	topics = Field() #Array of topics
+	date_published = Field()
+	author = Field()
+	language = Field()
+	images_url = Field() #Links to images in article. Optional for now
