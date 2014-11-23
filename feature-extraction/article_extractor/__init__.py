a1851d08ad51d9dc034385a32b925a22e2db386c
Ramzi Abdoch <ramzi.abdoch@gmail.com>
2014-11-23 18:03:54 -0500

    preparing merge

---
+++ feature-extraction/article_extractor/__init__.py
@@ -0,0 +1,17 @@
+# Author: Ramzi Abdoch
+
+class HTMLVectorizer(object):
+	def __init__(self):
+
+
+	def fit(self):
+		pass
+
+	def transform(self):
+		pass
+
+	def inverse_transform(self):
+		pass
+
+	def transform(self):
+		pass
