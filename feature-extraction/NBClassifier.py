39ea27cf4989499d3288953ac94b2c6ef3be6398
Ramzi Abdoch <ramzi.abdoch@gmail.com>
2014-12-25 15:11:59 -0500

    changed NBClassifier to class, defined classify function, fixing notebook

--- feature-extraction/NBClassifier.py
+++ feature-extraction/NBClassifier.py
@@ -2,11 +2,12 @@
 # Author: Ramzi Abdoch
 #
 # Usage:
-#	- python NBClassifier.py <folder_name>
+#	- NBClassifier.classify(folder_path) 
 #
 # Output:
 #   - Results of classifier on training data in folder
-#   - Files in folder are of file_type .annotation
+#
+# * Files in folder are of file_type .annotation
 
 # SciKit Learn: Naive Bayes + Classification Reporting
 from sklearn.naive_bayes import MultinomialNB
@@ -23,46 +24,50 @@ import os
 # HTMLVectorizer
 from article_extractor import htmlvectorizer
 
-# Initialize HTMLVectorizer
-hv = htmlvectorizer.HTMLVectorizer()
+class NBClassifier():
 
-# Files to open
-an_files = list()
+	def classify(self, folder):
 
-# Build list of dicts from .annotation files in folder (argv)
-for filename in os.listdir(sys.argv[1]):
-    path = os.path.join(sys.argv[1], filename)
-    an_files.append(path)
+		# Initialize HTMLVectorizer
+		hv = htmlvectorizer.HTMLVectorizer()
 
-# Build X & Y
-X = list()
-y = list()
+		# Files to open
+		an_files = list()
 
-# Get Matrix of Vectors for all files
-hv_result = hv.fit_transform(an_files)
-X = hv_result[0]
-y = hv_result[1]
+		# Build list of dicts from .annotation files in folder (argv)
+		for filename in os.listdir(folder):
+			path = os.path.join(folder, filename)
+			an_files.append(path)
 
-# Turn y into numpy array
-y = np.array(y)
+		# Build X & Y
+		X = list()
+		y = list()
 
-#############
-## TESTING ##
-#############
+		# Get Matrix of Vectors for all files
+		hv_result = hv.fit_transform(an_files)
+		X = hv_result[0]
+		y = hv_result[1]
 
-kf = KFold(len(y), n_folds=2, shuffle=True)
-print kf
+		# Turn y into numpy array
+		y = np.array(y)
 
-for train_index, test_index in kf:
-	print("TRAIN:", train_index, "TEST:", test_index)
-	X_train, X_test = X[train_index], X[test_index]
-	y_train, y_test = y[train_index], y[test_index]
+		#############
+		## TESTING ##
+		#############
 
-# Run SciKit Naive Bayes Classifier
-clf = MultinomialNB()
-clf.fit(X_train, y_train)
+		kf = KFold(len(y), n_folds=2, shuffle=True)
+		print kf
 
-# Run the classifier over the training data and report success
-y_pred = clf.predict(X_test)
+		for train_index, test_index in kf:
+			print("TRAIN:", train_index, "TEST:", test_index)
+			X_train, X_test = X[train_index], X[test_index]
+			y_train, y_test = y[train_index], y[test_index]
 
-print classification_report(y_test, y_pred)
+		# Run SciKit Naive Bayes Classifier
+		clf = MultinomialNB()
+		clf.fit(X_train, y_train)
+
+		# Run the classifier over the training data and report success
+		y_pred = clf.predict(X_test)
+
+		print classification_report(y_test, y_pred)
