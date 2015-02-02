8d64cbce6ad942668a9c81e7c9fb24b4bf109a75
Skillachie <dwaynecampbell13@gmail.com>
2015-02-02 12:45:24 -0500

    make example easier

--- nbquery/fetch_html_example.py
+++ nbquery/fetch_html_example.py
@@ -4,6 +4,6 @@ nb_cli = NBQueryCLI("island2.cs.columbia.edu")
 # Specify the url pattern of the source can be in regex
 # Location to save html source
 # Number of articles that you would like returned
-nb_cli.get_html_source("www.nytimes.com*","/tmp/html/",100)
+nb_cli.get_html_source("www.nytimes.com*","/tmp/",100)
 
 
