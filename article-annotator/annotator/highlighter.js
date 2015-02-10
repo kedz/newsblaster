2abb2793621253bcbbf207212c8816c46c734f9e
Ramzi Abdoch <ramzi.abdoch@gmail.com>
2015-02-10 13:09:46 -0500

    more work on filenames, finished now

--- article-annotator/annotator/highlighter.js
+++ article-annotator/annotator/highlighter.js
@@ -20,7 +20,7 @@ function handleFileSelect(evt) {
     // files is a FileList of File objects. List some properties.
     var output = [];
     for (var i = 0, f; f = files[i]; i++) {
-      fileName = f.name.substring(0, f.name.indexOf('.'));
+      fileName = f.name.substring(0, f.name.indexOf('.html'));
       output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
                   f.size, ' bytes, last modified: ',
                   f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a',
