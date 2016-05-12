c49c7d0df57ac457fffe095f4f415a8ed2c15b58
Skillachie <dwaynecampbell13@gmail.com>
2016-05-12 16:20:55 -0400

    remove sql orm

--- web/nbapp/app.py
+++ web/nbapp/app.py
@@ -3,8 +3,6 @@ import os
 import sys
 from flask import Flask, flash
 from flask import abort, jsonify, redirect, render_template, request, url_for
-from sqlalchemy import exc
-from sqlalchemy import func
 from nbapp import config
 from pprint import pprint
 
